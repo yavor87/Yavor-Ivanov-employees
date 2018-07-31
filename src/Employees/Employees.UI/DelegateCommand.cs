3d6828ffddb3bcdf8c9bef797c009c94bcf2a070
Yavor Ivanov <yavor87@gmail.com>
2018-07-31 04:15:09 +0300

    Adding UI

---
+++ src/Employees/Employees.UI/DelegateCommand.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Employees.UI
+{
+    public class DelegateCommand : ICommand
+    {
+        public DelegateCommand(Action execute)
+            : this(execute, null)
+        { }
+
+        public DelegateCommand(Action execute, Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        private readonly Predicate<object> canExecute;
+        private readonly Action execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (this.canExecute == null)
+            {
+                return true;
+            }
+
+            return this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class DelegateCommand<T> : ICommand
+    {
+        public DelegateCommand(Action<T> execute)
+            : this(execute, null)
+        { }
+
+        public DelegateCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        private readonly Predicate<T> canExecute;
+        private readonly Action<T> execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (this.canExecute == null)
+            {
+                return true;
+            }
+
+            return this.canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute((T)parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ src/Employees/Employees.UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Employees.Core;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Employees.UI.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            this.LoadFileCommand = new DelegateCommand(LoadFile);
+        }
+
+        private IReadOnlyCollection<EmployeeRecord> records;
+        private IEnumerable<ProjectEmployeeMatch> matches;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand LoadFileCommand { get; }
+
+        public IEnumerable<ProjectEmployeeMatch> Matches
+        {
+            get => this.matches;
+            private set
+            {
+                if (this.matches == value)
+                    return;
+
+                this.matches = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Matches)));
+            }
+        }
+
+        private async void LoadFile()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.CheckFileExists = true;
+            if (ofd.ShowDialog() != true)
+            {
+                return;
+            }
+
+            try
+            {
+                using (Stream fs = ofd.OpenFile())
+                {
+                    EmployeeReader reader = new EmployeeReader();
+                    this.records = await reader.ReadRecordsAsync(fs);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error reading file");
+                return;
+            }
+
+            ProjectEmployeeMatcher matcher = new ProjectEmployeeMatcher();
+            this.Matches = matcher.FindMatches(this.records);
+        }
+    }
+}
